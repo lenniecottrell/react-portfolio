15d7c4c55d746a89cfdb0b41faec84ec9bc1a25c
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-02-16 16:03:13 -0800

    add about section animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,8 @@ import home1 from "../img/home1.png"
 //styles
 import {GS_Section, GS_Description, GS_Image, GS_Hide} from '../styles'
 import {motion} from 'framer-motion'
+import {titleAnim, fade, photoAnim} from '../animation'
+import Wave from './Wave'
 
 const AboutSection = () => {
 
@@ -14,29 +16,30 @@ const AboutSection = () => {
       <GS_Description>
         <motion.div>
           <GS_Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
                 We work to make
             </motion.h2>
           </GS_Hide>
           <GS_Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
                your <span>dreams</span> come
             </motion.h2>
           </GS_Hide>
           <GS_Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               true.
             </motion.h2>
           </GS_Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We have amazing skillz
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </GS_Description>
         <GS_Image>
-          <img src={home1} alt="guy with a camera"/>
+          <motion.img variants={photoAnim} initial='hidden' animate='show' src={home1} alt="guy with a camera"/>
         </GS_Image>
+        <Wave />
     </GS_Section>
   )
 }
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,16 +7,25 @@ import theRacer from '../img/theracer-small.png'
 import goodTimes from '../img/goodtimes-small.png'
 //animations
 import {motion} from 'framer-motion'
-import {pageAnimation} from "../animation"
+import {pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer} from "../animation"
 
 const OurWork = () => {
   return (
     <S_Work variants={pageAnimation} initial="hidden" animate="show" exit="exit" style={{background: '#fff'}}>
+      {/* these get staggered because of the staggerChildren property in pageAnimation */}
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
       <S_Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="The Athlete"/>
+          <S_Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="The Athlete"/>
+          </S_Hide>
         </Link>
       </S_Movie>
       <S_Movie>
@@ -56,9 +65,33 @@ const S_Movie = styled.div`
   padding-bottom: 10rem;
   .line{
     height: 0.5rem;
-    background: #ccc;
+    background: #23d997;
     margin-bottom: 3rem;
   }
+`;
+
+const S_Hide = styled.div`
+  overflow: hidden;
+`;
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 export default OurWork
