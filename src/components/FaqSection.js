c0e81979bf220485ea27361eff2fda8b192e15b3
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-02-17 08:55:59 -0800

    add toggle component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,46 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
-import {GS_Section} from '../styles'
+import React from "react";
+import styled from "styled-components";
+import { GS_Section } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
     <Faq>
-      <h2>Any Questions? <span>FAQ</span></h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, laudantium?</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, laudantium?</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, laudantium?</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, laudantium?</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title="How do I start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, laudantium?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, laudantium?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, laudantium?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What products do you offer?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, laudantium?
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
-  )
-}
+  );
+};
 
 const Faq = styled(GS_Section)`
   display: block;
@@ -67,7 +77,6 @@ const Faq = styled(GS_Section)`
       padding: 2rem 0rem;
     }}
   }
-`
+`;
 
-
-export default FaqSection
+export default FaqSection;
